e2634f00957ca7c6ec68bab176b5a1f889ab35fe
nyahyun <hamnahyun@outlook.com>
2022-07-06 22:14:43 +0900

    square extent basic

---
+++ 09_struct/square_extent_basic/target/debug/deps/square_extent_basic-8b11e0a46c06b35f.d
@@ -0,0 +1,5 @@
+/Users/hamnahyun/progarm/Rust/09_struct/square_extent_basic/target/debug/deps/square_extent_basic-8b11e0a46c06b35f: src/main.rs
+
+/Users/hamnahyun/progarm/Rust/09_struct/square_extent_basic/target/debug/deps/square_extent_basic-8b11e0a46c06b35f.d: src/main.rs
+
+src/main.rs:
---
+++ 09_struct/square_extent_basic/target/debug/square_extent_basic.d
@@ -0,0 +1,1 @@
+/Users/hamnahyun/progarm/Rust/09_struct/square_extent_basic/target/debug/square_extent_basic: /Users/hamnahyun/progarm/Rust/09_struct/square_extent_basic/src/main.rs
